482352c56cbeeb3186bed5a8ac6afbaa90f90e33
Alexis Gil <alexisgilcabrera@gmail.com>
2025-04-08 10:12:14 +0100

    Traducciones

---
+++ agrolanza/lang/es/profile.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'profile_information' => 'Información del perfil',
+    'update_profile_information' => 'Actualiza la información de tu perfil y la dirección de correo electrónico.',
+    'name' => 'Nombre',
+    'email' => 'Correo electrónico',
+    'email_unverified' => 'Tu dirección de correo electrónico no está verificada.',
+    'resend_verification' => 'Haz clic aquí para reenviar el correo de verificación.',
+    'verification_link_sent' => 'Se ha enviado un nuevo enlace de verificación a tu correo electrónico.',
+    'save' => 'Guardar',
+    'saved' => 'Guardado.',
+];
